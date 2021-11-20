12003eb85be8f0e82c9f5020fda373e56da553f8
Ishan Jain <56102033+ishanjain1502@users.noreply.github.com>
2021-11-20 09:51:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const PORT = 3030;
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const express = require('express');
+const app = express();
+const cors = require('cors')
+app.use(cors())
+
+const url = ''; // ADD URL to be SCRAPED HERE 
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+
+        $('// add the class here which you want to find ', html).each(function () {
+            const title = $(this).text()
+            const pageUrl = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                pageUrl
+            })
+        })
+        console.log(articles);
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
